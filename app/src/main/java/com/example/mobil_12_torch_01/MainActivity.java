490ada50b0031b962844e0b27762be4d6906c6e8
makar <makar.peter98@gmail.com>
2025-05-06 10:50:10 +0200

    Mobil 12 Torch alap

---
+++ app/src/main/java/com/example/mobil_12_torch_01/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mobil_12_torch_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String myAppID = "hu.unideb.mobil_123456789";
+    public static final String TORCH_KEY = myAppID + ".TORCH_KEY";
+    public static final String SHR_FILENAME = myAppID + ".SHR_FILENAME";
+
+    //static final String TORCH_KEY = BuildConfig.APPLICATION_ID + ".TORCH_KEY";
+    //static final String SHR_FILENAME = BuildConfig.APPLICATION_ID + ".SHR_FILENAME";
+    private Button torchButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private boolean torch;
+
+    public void torchAction(View view) {
+
+    }
+}
---
+++ app/src/main/java/com/example/mobil_12_torch_01/TorchWidget.java
@@ -0,0 +1,44 @@
+package com.example.mobil_12_torch_01;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class TorchWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
